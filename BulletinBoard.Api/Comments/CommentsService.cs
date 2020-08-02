df66fcf73399d1c41a74dba5eaf05405930859e6
Alexandra <oleksandra.kryvko@nure.ua>
2020-08-02 13:36:41 +0300

    Add BulletinBoard.Api project

---
+++ BulletinBoard.Api/Comments/CommentsService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulletinBoard.Api.Comments
+{
+    public class CommentsService
+    {
+    }
+}
